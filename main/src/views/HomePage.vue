6e4cf4c87250bfe2d26500e68f1ddfeb0cb5947c
Sammi Chua <samischua00@gmail.com>
2025-10-07 15:50:27 +0800

    some css

--- main/src/views/HomePage.vue
+++ main/src/views/HomePage.vue
@@ -1,21 +1,28 @@
+
+
 <template>
-    <div class="home">
+    <div class="home container-fluid">
       <h1>Welcome to PowerTrack!</h1>
       <p>Your smart workout companion</p>
       
-      <div class="row mt-5">
-        <div class="col-lg-6">
+      <div class="row">
+        <div class = "col-lg-1"></div>
+        <div class="col-lg-5 box ">
           <h2>Generate Smart Workouts</h2>
           <p>Get personalised workout routines based on your time, goals, and fitness level.</p>
-          <button class="btn btn-primary btn-lg">Start Your Workout</button>
+          <button class="btn1 ">Start Your Workout</button>
         </div>
-        <div class="col-lg-6">
+        <div class="col-lg-5 box ">
+          <h2>Progress Tracking</h2>
+          <p>Visualise your progress over time with charts and statistics.</p>
+          <button class="btn2  ">Join usss Now</button>
         </div>
+        <div class = "col-lg-1"></div>
       </div>
     </div>
   </template>
   
-  <script setup>
+  <script setup> import '../styles/main.css'; 
   </script>
   
   <style scoped></style>
