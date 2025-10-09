b1631d010a96bfae5b913d06bc15c95a6e5d7213
Sammi Chua <samischua00@gmail.com>
2025-10-09 11:58:42 +0800

    not significant update

--- main/src/views/HomePage.vue
+++ main/src/views/HomePage.vue
@@ -10,12 +10,12 @@
         <div class="col-lg-5 box ">
           <h2>Generate Smart Workouts</h2>
           <p>Get personalised workout routines based on your time, goals, and fitness level.</p>
-          <button class="btn1 ">Start Your Workout</button>
+          <button class="btn1">Start Your Workout</button>
         </div>
         <div class="col-lg-5 box ">
           <h2>Progress Tracking</h2>
           <p>Visualise your progress over time with charts and statistics.</p>
-          <button class="btn2  ">Join usss Now</button>
+          <button class="btn2  ">Join us Now</button>
         </div>
         <div class = "col-lg-1"></div>
       </div>
